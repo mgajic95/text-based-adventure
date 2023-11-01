898107623e6a9f0f5f0fe4a6076ee7a6d31ce169
mgajic95 <mgajic21234@gmail.com>
2023-11-01 17:47:17 +0100

    Updated logics and more interaction added

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import time
+import random
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.health = 100
         self.inventory = []
+        self.score = 0
 
     def display_status(self):
         print(f"\n{self.name}'s Status:")
         print(f"Health: {self.health}")
         print("Inventory:", ", ".join(self.inventory))
+        print(f"Score: {self.score}")
 
 def introduction():
-    print("Welcome to the Adventure Game!")
-    print("You find yourself in a mysterious land. Your goal is to survive and explore.")
+    print("Welcome to the Advanced Adventure Game!")
+    print("You find yourself in a dangerous land. Your goal is to survive, explore, and earn points.")
     print("Choose your actions wisely!")
 
 def make_choice(choices):
@@ -31,37 +34,62 @@ def make_choice(choices):
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-def battle():
-    print("\nA wild monster appears!")
+def battle(player, enemy_difficulty):  # Correct the function signature
+    print(f"\nA wild monster appears with difficulty level {enemy_difficulty}!")
     time.sleep(1)
 
     choices = ["Fight", "Run"]
     player_choice = make_choice(choices)
 
     if player_choice == 1:  # Fight
+        player_damage = random.randint(10, 20)
+        enemy_damage = random.randint(5, 15 + enemy_difficulty)
+
         print("You engage in a fierce battle!")
-        # Add battle logic here based on player's stats
-        print("You defeated the monster!")
+        print(f"You dealt {player_damage} damage to the monster.")
+        print(f"The monster dealt {enemy_damage} damage to you.")
+
+        if player_damage > enemy_damage:
+            print("You defeated the monster!")
+            return True
+        else:
+            print("You were defeated by the monster!")
+            return False
 
     elif player_choice == 2:  # Run
         print("You attempt to run away.")
-        # Add run logic here based on player's stats
-        print("You successfully escaped from the monster.")
 
-def explore():
-    print("\nYou enter a dark cave.")
+        run_success_probability = min(0.5, 0.2 + 0.1 * enemy_difficulty)
+        if random.random() < run_success_probability:
+            print("You successfully escaped from the monster.")
+            return True
+        else:
+            print("You failed to escape. Prepare for battle!")
+            return False
+
+def explore(player):
+    print("\nYou enter a mysterious area.")
     time.sleep(1)
 
-    choices = ["Enter deeper into the cave", "Go back"]
+    choices = ["Search for treasure", "Rest and regain health", "Continue exploring"]
     player_choice = make_choice(choices)
 
-    if player_choice == 1:  # Enter deeper into the cave
-        print("You discover a hidden treasure!")
-        # Add treasure logic here based on player's stats
-        print("You add the treasure to your inventory.")
+    if player_choice == 1:  # Search for treasure
+        treasure_found_probability = min(0.8, 0.1 + 0.1 * player.score)
+        if random.random() < treasure_found_probability:
+            print("You discover a hidden treasure!")
+            player.inventory.append("Treasure")
+            player.score += 10
+        else:
+            print("You search but find nothing.")
 
-    elif player_choice == 2:  # Go back
-        print("You decide not to risk it and return to the outside.")
+    elif player_choice == 2:  # Rest and regain health
+        health_gain = random.randint(10, 30)
+        print(f"You rest and regain {health_gain} health.")
+        player.health = min(100, player.health + health_gain)
+
+    elif player_choice == 3:  # Continue exploring
+        print("You continue exploring.")
 
 def main():
     introduction()
@@ -69,17 +97,48 @@ def main():
     player_name = input("Enter your name: ")
     player = Player(player_name)
 
-    while True:
+    enemy_difficulty = 1
+
+    while player.health > 0:
         player.display_status()
 
-        battle()
-        if player.health <= 0:
-            print("You have been defeated! Game Over.")
+        if not battle(player, enemy_difficulty):
+            print("Game Over. You can try again.")
             break
 
-        explore()
+        explore(player)  # Pass the 'player' object to the explore function
+
+        enemy_difficulty += 1
+
         if player.health <= 0:
             print("You have been defeated! Game Over.")
+
+def main():
+    while True:  # Add a loop for replaying the game
+        introduction()
+
+        player_name = input("Enter your name: ")
+        player = Player(player_name)
+
+        enemy_difficulty = 1
+
+        while player.health > 0:
+            player.display_status()
+
+            if not battle(player, enemy_difficulty):
+                print("Game Over. You can try again.")
+                break
+
+            explore(player)
+
+            enemy_difficulty += 1
+
+            if player.health <= 0:
+                print("You have been defeated! Game Over.")
+                break
+
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != 'yes':
             break
 
 if __name__ == "__main__":
