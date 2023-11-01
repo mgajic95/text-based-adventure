4384c32dd6abf4aadc4612c07d973d9fd18b6dab
mgajic95 <mgajic21234@gmail.com>
2023-11-01 17:28:43 +0100

    Battle/run code

--- main.py
+++ main.py
@@ -20,3 +20,33 @@ def make_choice(choices):
     print("\nChoose an action:")
     for index, choice in enumerate(choices, start=1):
         print(f"{index}. {choice}")
+
+    while True:
+            try:
+                choice = int(input("Enter the number of your choice: "))
+                if 1 <= choice <= len(choices):
+                    return choice
+                else:
+                    print("Invalid choice. Please enter a valid number.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+
+def battle():
+    print("\nA wild monster appears! ")
+    time.sleep(1)
+
+    choices = ["Fight", "Run"]
+    player_choice = make_choice(choices)
+
+    if player_choice == 1:
+        print("You engage in fierce battle!")
+
+        print("You defeated the monster!!")
+
+    elif player_choice == 2: #run
+        print("You run away from the danger...")
+
+        print("You successfully escaped from the monster.")
+
+        
