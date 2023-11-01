9fdf1139e86ca971da36ca65a169423332144b0e
mgajic95 <mgajic21234@gmail.com>
2023-11-01 17:39:02 +0100

    Finished code with stats, healt and inventory

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Player:
         self.inventory = []
 
     def display_status(self):
-        print(f"\n{name}'s Status:")
+        print(f"\n{self.name}'s Status:")
         print(f"Health: {self.health}")
         print("Inventory:", ", ".join(self.inventory))
 
@@ -22,31 +22,65 @@ def make_choice(choices):
         print(f"{index}. {choice}")
 
     while True:
-            try:
-                choice = int(input("Enter the number of your choice: "))
-                if 1 <= choice <= len(choices):
-                    return choice
-                else:
-                    print("Invalid choice. Please enter a valid number.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-
+        try:
+            choice = int(input("Enter the number of your choice: "))
+            if 1 <= choice <= len(choices):
+                return choice
+            else:
+                print("Invalid choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 def battle():
-    print("\nA wild monster appears! ")
+    print("\nA wild monster appears!")
     time.sleep(1)
 
     choices = ["Fight", "Run"]
     player_choice = make_choice(choices)
 
-    if player_choice == 1:
-        print("You engage in fierce battle!")
+    if player_choice == 1:  # Fight
+        print("You engage in a fierce battle!")
+        # Add battle logic here based on player's stats
+        print("You defeated the monster!")
 
-        print("You defeated the monster!!")
-
-    elif player_choice == 2: #run
-        print("You run away from the danger...")
-
+    elif player_choice == 2:  # Run
+        print("You attempt to run away.")
+        # Add run logic here based on player's stats
         print("You successfully escaped from the monster.")
 
-        
+def explore():
+    print("\nYou enter a dark cave.")
+    time.sleep(1)
+
+    choices = ["Enter deeper into the cave", "Go back"]
+    player_choice = make_choice(choices)
+
+    if player_choice == 1:  # Enter deeper into the cave
+        print("You discover a hidden treasure!")
+        # Add treasure logic here based on player's stats
+        print("You add the treasure to your inventory.")
+
+    elif player_choice == 2:  # Go back
+        print("You decide not to risk it and return to the outside.")
+
+def main():
+    introduction()
+
+    player_name = input("Enter your name: ")
+    player = Player(player_name)
+
+    while True:
+        player.display_status()
+
+        battle()
+        if player.health <= 0:
+            print("You have been defeated! Game Over.")
+            break
+
+        explore()
+        if player.health <= 0:
+            print("You have been defeated! Game Over.")
+            break
+
+if __name__ == "__main__":
+    main()
